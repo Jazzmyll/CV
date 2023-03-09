c870f97434031fb469ae51c354ec1db92ad4d58d
Jazzmyll <127032860+Jazzmyll@users.noreply.github.com>
2023-03-09 18:22:49 +0800

    Add files via upload

---
+++ MyCV.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My CV</title>
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="MyCV.css">
+    </head>
+
+    <body>
+        <div class="container">
+            <div class="left_side">
+                
+                <div class="profile">
+                    <div class="image">
+                        <img src="profile.jpg">
+                    </div>
+                    <h4>Jazzmyll Sia</h4>
+                    <h5>Quality Analyst</h5>
+                    <button class="btn" onclick="PDF();">Download CV</button>
+                </div>
+
+            <div class="left_items">
+                <h4 class="title">Personal Information</h4>
+                <ul>
+                    <li>
+                        <span class="icons">
+                            <i class='bx bx-home'></i>
+                        </span>
+                        <span class="text">0006 Unit A Doña Juliana II Subd. Sta. Lucia, Pasig City</span>
+                    </li>
+                    <li>
+                        <span class="icons">
+                             <i class='bx bx-male-female'></i>
+                        </span>
+                        <span class="text">Female</span>
+                    </li>
+                    <li>
+                        <span class="icons">
+                             <i class='bx bx-party'></i>
+                        </span>
+                        <span class="text">June 10, 2002</span>
+                    </li>
+                    <li>
+                        <span class="icons">
+                             <i class='bx bxs-calendar-plus'></i>
+                        </span>
+                        <span class="text">20</span>
+                    </li>
+                </ul>
+
+                <h4 class="title">Contact Information</h4>
+                <ul>
+                    <li>
+                        <span class="icons">
+                            <i class='bx bxs-envelope' ></i>
+                        </span>
+                        <span class="text">sia_jazzmyll@plpasig.edu.ph</span>
+                    </li>
+                    <li>
+                        <span class="icons">
+                             <i class='bx bxs-contact' ></i>
+                        </span>
+                        <span class="text">09380469515</span>
+                    </li>
+                    <li>
+                        <span class="icons">
+                            <i class='bx bxl-facebook-circle' ></i>
+                        </span>
+                        <span class="text">Jazzmyll@facebook</span>
+                    </li>
+                </ul>
+            <div class="left_items">
+                <h4 class="title">Awards and Recognition</h4>
+                <ul>
+                    <li>
+                        <h5 class="dates">2017-2018</h5>
+                        <h4 class="text">Academic Excellence Award with Honors</h4>
+                        <h4 class="text">Eusebio High School</h4>
+                    </li>
+                    <li>
+                        <h5 class="dates">2018-2020</h5>
+                        <h4 class="text">Academic Excellence Award with Honors</h4>
+                        <h4 class="text">Eusebio High School</h4>
+                    </li>
+                    <li>
+                        <h5 class="dates">2020-2023</h5>
+                        <h4 class="text">Academic Excellence Award Dean Lister</h4>
+                        <h4 class="text">Pamantasan ng Lungsod ng Pasig</h4>
+                    </li>
+                </ul>
+            </div>
+            <div class="left_items">
+                <h4 class="title">Skills and Languages</h4>
+                <ul class="languages">
+                    <li>
+                        <span class="text">Versatile</span>
+                        <div class="percent">
+                            <div style="width: 90%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text">CSS</span>
+                        <div class="percent">
+                            <div style="width: 76%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text">Fast-learner</span>
+                        <div class="percent">
+                            <div style="width: 86%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text">HTML</span>
+                        <div class="percent">
+                            <div style="width: 80%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text">Hardworking</span>
+                        <div class="percent">
+                            <div style="width: 96%"></div>
+                        </div>
+                    </li>
+                    <li>
+                        <span class="text">Python</span>
+                        <div class="percent">
+                            <div style="width: 85%"></div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            </div>
+            </div>
+
+            <div class="right_side">
+                <div class="right_items">
+                    <h3 class="title">About Me</h3>
+                    <p>My name is Jazzmyll Sia, and I am studying at Pamantasan ng Lungsod ng Pasig. I am a dedicated and hardworking individual. I can work under pressure, follow instructions and deliver quality results. My objective is to secure a career that can help me to grow and support my needs while helping the success of the company.</p>
+                </div>
+                <div class="right_items">
+                    <h4 class="title">Experience</h4>
+
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>May 2021 - Dec 2022</h5>
+                            </div>
+                            <div class="description">
+                                <h4>Concentrix</h4>
+                                <p>Customer Service Representative(voice) and Credit Control(back office)</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>Aug 2020 - Oct 2020</h5>
+                            </div>
+                            <div class="description">
+                                <h4>SM Hypermarket</h4>
+                                <p>Cashier</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>Dec 6, 2020 - Dec 21, 2020</h5>
+                            </div>
+                            <div class="description">
+                                <h4>Pasig City Children's Hospita</h4>
+                                <p>School Work Immersion-Medical Records Department</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="right_items">
+                    <h4 class="title">Education</h4>
+
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>2020 - Present Year</h5>
+                            </div>
+                            <div class="description">
+                                <h4>Pamantasan ng Lungsod ng Pasig</h4>
+                                <p>Tertiary Education</p>
+                                <p>12-B Alcalde Jose, Pasig, 1600 Metro Manila</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>2018 - 2020</h5>
+                            </div>
+                            <div class="description">
+                                <h4>Eusebio High School</h4>
+                                <p>Senior High School</p>
+                                <p>C. Raymundo Avenue Rosario, Pasig City</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>2014 - 2018</h5>
+                            </div>
+                            <div class="description">
+                                <h4>Eusebio High School</h4>
+                                <p>Junior High School</p>
+                                <p>C. Raymundo Avenue Rosario, Pasig City</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="experience">
+                        <div class="time_line">
+                            <span class="time_point"></span>
+                            <span class="time_road"></span>
+                        </div>
+                        <div class="content">
+                            <div class="interval">
+                                <h5>2008 - 2014</h5>
+                            </div>
+                            <div class="description">
+                                <h4>Rosario Elementary School</h4>
+                                <p>Elementary</p>
+                                <p>Dr. Sixto Avenue Rosario, Pasig City</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right_items">
+                    <h4 class="title">Hobbies</h4>
+                    <ul class="hobby">
+                        <li><i class='bx bxs-book' ></i>Reading</li>
+                        <li><i class='bx bxs-leaf' ></i>Planting</li>
+                        <li><i class='bx bxs-tv' ></i>Watching</li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <script type="text/javascript">
+            function PDF(){
+                const e = document.querySelector(".container");
+
+                e.style.maxWidth = "700px";
+
+                const opt = {
+                    filename: 'resume.pdf',
+                    image: {
+                        type: "jpg",
+                        quality: 0.99
+                    },
+                    margin: 0,
+                    jsPDF: {
+                        unit: "pt",
+                        format: "letter",
+                        orientation:"portrait"
+                    },
+                    pagebreak: {
+                        mode: ["avoid-all", "css", "legacy"]
+                    }
+                };
+            html2pdf().set(opt).from(e).save();
+
+            setTimeout(() => {e.style.maxWidth = "1400px";}, 2000);
+            }
+        </script>
+
+
+    </body>
+</html>
